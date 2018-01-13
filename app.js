eb4d1cc8b89e7c97fad1d1b64146560bf6af203d
ace1122sp <ace11.space22@gmail.com>
2018-01-13 20:46:07 +0100

    alpha version finished

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
 
+const controller = require('./controller');
+
 const port = 9000;
 
 const app = express();
@@ -11,15 +13,17 @@ const routeOptions = {
   root: __dirname + '/public'
 };
 
-
+app.use(helmet());
 app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile('index.html', routeOptions);
 });
+
+app.get('/:param', controller);
 
 
 
 
 app.listen(port, () => {
   console.log(`Server listening at port ${port}...`);
-})
+});
---
+++ controller.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const date_generator = (req, res) => {
+  const param = req.params.param;
+
+  api = getDate(param);
+  res.send(api);
+}
+
+const getDate = param => {
+  let date, api, dateObj;
+
+  const options = {year: 'numeric', month: 'long', day: 'numeric'};
+  const notTimestamp = isNaN(Number(param));
+
+  if(notTimestamp) {
+    date = new Date(param);
+  } else {
+    let timestamp = parseInt(param);
+    date = new Date(timestamp);
+  }
+
+  const unix = date.getTime();
+  let natural = date.toLocaleDateString('en-US', options);
+  if(natural === "Invalid Date") natural = null;
+
+  api = {
+    unix,
+    natural
+  }
+
+  return api;
+}
+
+module.exports = date_generator;
